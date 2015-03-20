ee2fcd7f096f37d34982dd287d3bb1f0d4bc42cb
NGUYEN VAN LUONG <luongnv89@gmail.com>
2015-03-20 16:12:32 +0100

    Add Pcap learning

---
+++ pcap-learning/pcap-list-device.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <pcap.h>
+
+int main(int argc, char *argvp[]){
+	char *dev, errbuf[PCAP_ERRBUF_SIZE];
+	dev = pcap_lookupdev(errbuf);
+	if(dev==NULL){
+		fprintf(stderr,"Couldn't find default device: %s\n",errbuf);
+		return(2);
+	}
+	printf("Device: %s\n",dev);
+	return(0);
+}
