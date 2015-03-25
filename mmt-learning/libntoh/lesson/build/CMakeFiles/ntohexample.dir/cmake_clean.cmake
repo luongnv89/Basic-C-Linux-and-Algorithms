d265ec5c3e182e048a895d8e8d68d99bf477c33c
Luong Nguyen <luongnv89@gmail.com>
2015-03-25 16:03:56 +0100

    Update from virtual machine

---
+++ mmt-learning/libntoh/lesson/build/CMakeFiles/ntohexample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ntohexample.dir/second-app.c.o"
+  "ntohexample.pdb"
+  "ntohexample"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/ntohexample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
